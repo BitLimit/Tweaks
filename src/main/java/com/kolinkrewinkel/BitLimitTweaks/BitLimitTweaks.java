5faafa6014fef3109850e7ddb5b8dd0cfafdbdeb
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2012-08-08 01:06:10 -0700

    remove subtraction

--- src/main/java/com/kolinkrewinkel/BitLimitTweaks/BitLimitTweaks.java
+++ src/main/java/com/kolinkrewinkel/BitLimitTweaks/BitLimitTweaks.java
@@ -27,9 +27,9 @@ public class BitLimitTweaks extends JavaPlugin {
                 if (!world.hasStorm()) {
                     this.plugin.getServer().broadcastMessage(ChatColor.GREEN + "Rain decremented!");
                     int weatherDuration = world.getWeatherDuration();
-                    this.plugin.getServer().broadcastMessage(ChatColor.RED + Integer.toString(world.getWeatherDuration()));
-                    world.setWeatherDuration(weatherDuration + 300);
-                    this.plugin.getServer().broadcastMessage(ChatColor.RED + Integer.toString(world.getWeatherDuration()));
+                    this.plugin.getServer().broadcastMessage(ChatColor.RED + "Old value: " + Integer.toString(world.getWeatherDuration()));
+                    world.setWeatherDuration(weatherDuration + 0);
+                    this.plugin.getServer().broadcastMessage(ChatColor.RED + "New Value: " + Integer.toString(world.getWeatherDuration()));
                 } else {
                     this.plugin.getServer().broadcastMessage(ChatColor.RED + "Timer untouched, it's raining!");
                     this.plugin.getServer().broadcastMessage(ChatColor.RED + Integer.toString(world.getWeatherDuration()));
