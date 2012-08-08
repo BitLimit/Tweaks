9b93679f140d1ea2a81711bc7c4d0b7c37e918e9
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2012-08-07 23:02:13 -0700

    ?

--- src/main/java/com/kolinkrewinkel/BitLimitTweaks/BitLimitTweaks.java
+++ src/main/java/com/kolinkrewinkel/BitLimitTweaks/BitLimitTweaks.java
@@ -3,6 +3,8 @@ package com.kolinkrewinkel.BitLimitTweaks;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.ChatColor;
+import org.bukkit.World;
+import org.bukkit.Server;
 import java.util.*;
 
 public class BitLimitTweaks extends JavaPlugin {
@@ -13,18 +15,8 @@ public class BitLimitTweaks extends JavaPlugin {
 
         this.getCommand("tweaks").setExecutor(new TweaksCommandExecutor(this));
 
-        this.getServer().getScheduler().scheduleAsyncRepeatingTask(this, new Runnable() {
-            public void run() {
-                World world = this.getServer().getWorld(this.getConfig().getString("world"));
-                if (!world.hasStorm()) {
-                    this.getServer().broadcastMessage(ChatColor.GREEN + "Rain decremented!");
-                    int weatherDuration = world.getWeatherDuration();
-                    world.setWeatherDuration(weatherDuration + 6000);
-                } else {
-                    this.getServer().broadcastMessage(ChatColor.RED + "Timer untouched, it's raining!");
-                }
-            }
-        }, 0L, 1200L);
+        BitLimitTweaksRepeatingTask task = new BitLimitTweaksRepeatingTask();
+        this.getServer().getScheduler().scheduleSyncRepeatingTask(this, task, 0L, 1200L);
     }
 
     @Override
